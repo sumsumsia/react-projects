f3b2707acec6e47526918503b2dc4d9d52f58cf5
sumsumsia <luckyhandryan98@gmail.com>
2025-08-25 00:10:06 +0700

    25/8/25 push

--- src/components/ContactItemBody.jsx
+++ src/components/ContactItemBody.jsx
@@ -4,7 +4,7 @@ function ContactItemBody({ name, tag }) {
     return (
         <div className="contact-item__body">
             <h3 className="contact-item__title">{name}</h3>
-            <p className="contact-item__username">@{tagh}</p>
+            <p className="contact-item__username">@{tag}</p>
         </div>
     );
 }
